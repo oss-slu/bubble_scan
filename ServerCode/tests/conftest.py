6886bbf92aa4581b09860ed4cd02b70e88604944
Nischita Nannapaneni <nischitanannapaneni@Nischitas-MacBook-Air.local>
2024-02-26 14:18:13 -0600

    more linting

--- ServerCode/tests/conftest.py
+++ ServerCode/tests/conftest.py
@@ -2,8 +2,19 @@ import pytest
 
 from application.app import create_app
 
+"""
+Module Docstring: Describe purpose here
+"""
+
 @pytest.fixture
 def app():
+
+    """
+    Fixture to set up the 'app' object for testing.
+
+    Returns: 
+        Flask App: The Flask application instance.
+    """
     app = create_app("testing")
-    
+
     return app
