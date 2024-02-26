b5ee9de8bd16d10166819246593319375a7be070
Nischita Nannapaneni <nischitanannapaneni@Nischitas-MacBook-Air.local>
2024-02-26 14:25:40 -0600

    linting

--- ServerCode/tests/test_responses.py
+++ ServerCode/tests/test_responses.py
@@ -13,12 +13,18 @@ GENERIC_RESPONSE_MESSAGE = "This is a response"
 
 
 def test_response_success_is_true():
+    """
+    Test if ResponseSuccess returns True when converted to a boolean.
+    """
     response = ResponseSuccess(SUCCESS_VALUE)
 
     assert bool(response) is True
 
 
 def test_response_failure_is_false():
+    """
+    Test if ResponseFailure returns False when converted to a boolean.
+    """
     response = ResponseFailure(
         GENERIC_RESPONSE_TYPE, GENERIC_RESPONSE_MESSAGE
     )
@@ -27,6 +33,9 @@ def test_response_failure_is_false():
 
 
 def test_response_success_has_type_and_value():
+    """
+    Test if ResponseSuccess has the correct type and value.
+    """
     response = ResponseSuccess(SUCCESS_VALUE)
 
     assert response.type == ResponseTypes.SUCCESS
@@ -34,6 +43,9 @@ def test_response_success_has_type_and_value():
 
 
 def test_response_failure_has_type_and_message():
+    """
+    Test if ResponseFailure has the correct type, message, and value.
+    """
     response = ResponseFailure(
         GENERIC_RESPONSE_TYPE, GENERIC_RESPONSE_MESSAGE
     )
@@ -47,6 +59,9 @@ def test_response_failure_has_type_and_message():
 
 
 def test_response_failure_initialisation_with_exception():
+    """
+    Test if ResponseFailure initializes correctly with an exception.
+    """
     response = ResponseFailure(
         GENERIC_RESPONSE_TYPE, Exception("Just an error message")
     )
@@ -57,6 +72,9 @@ def test_response_failure_initialisation_with_exception():
 
 
 def test_response_failure_from_empty_invalid_request():
+    """
+    Test if building a response from an empty invalid request works as expected.
+    """
     response = build_response_from_invalid_request(
         ScantronListInvalidRequest()
     )
@@ -66,6 +84,9 @@ def test_response_failure_from_empty_invalid_request():
 
 
 def test_response_failure_from_invalid_request_with_errors():
+    """
+    Test if building a response from an invalid request with errors works as expected.
+    """
     request = ScantronListInvalidRequest()
     request.add_error("path", "Is mandatory")
     request.add_error("path", "can't be blank")
