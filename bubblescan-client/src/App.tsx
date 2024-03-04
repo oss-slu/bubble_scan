a73eabca9b1c408668203e3c55918853bfcdeb9a
Nischita Nannapaneni <nischitanannapaneni@Nischitas-MacBook-Air.local>
2024-03-04 15:06:20 -0600

    merging

--- bubblescan-client/src/App.tsx
+++ bubblescan-client/src/App.tsx
@@ -1,40 +1,9 @@
-import React, { useEffect, useState } from 'react';
 import ListGroup from "./components/ListGroup";
 import InputComponent from "./components/InputComponent";
 import FileUploadComponent from "./components/FileUploadComponent";
 import "./App.css";
 
 function App() {
-  const [data, setData] = useState<string>('');
-  const [message, setMessage] = useState<string>('');
-  const [response, setResponse] = useState<string>('');
-
-  // Fetch initial data from Flask
-  useEffect(() => {
-    fetch('http://localhost:5000/api/data')
-      .then(response => response.json())
-      .then(data => setData(data.message))
-      .catch(error => console.error('Error fetching data:', error));
-  }, []);
-
-  // Function to send message to Flask
-  const sendMessage = async () => {
-    try {
-      const res = await fetch('http://localhost:5000/api/message', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ message }),
-      });
-      const data = await res.json();
-      setResponse(data.message);
-    } catch (error) {
-      console.error('Error sending message:', error);
-      setResponse('Failed to send message.');
-    }
-  };
-
   return (
     <div>
       <h1>Welcome To Bubble Scan</h1>
@@ -54,6 +23,7 @@ function App() {
         <button onClick={sendMessage}>Send Message</button>
         {response && <p>Response from sending message: {response}</p>}
       </div>
+      <ListGroup />
     </div>
   );
 }
