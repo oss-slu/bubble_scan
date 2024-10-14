e7d74e253d3f3907a253fa78539844bba1d3e8bc
Kate Holdener <kate.holdener@slu.edu>
2024-10-14 14:11:42 -0500

    added deployment script and upldated AppServer to allow CORS for localhost:5001

---
+++ deploy_on_rpi.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# create a static directory in the application server
+# front-end files will be placed there
+mkdir -p ServerCode/application/static
+
+# generate front-end files
+cd bubblescan-client
+npm run build
+
+# copy front-end files to the application server
+cp -r dist/* ../ServerCode/application/static
+
+# start the app
+cd ../ServerCode
+python application/AppServer.py
