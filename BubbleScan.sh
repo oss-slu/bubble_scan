dbc9e95574e819ef9bac4db58d7c9a8e5f3f66f7
mylesc <myles.correa@slu.edu>
2024-11-12 00:24:06 -0600

    Quick typo change in BubbleScan.sh

--- BubbleScan.sh
+++ BubbleScan.sh
@@ -54,7 +54,7 @@ cd ..
 mkdir -p ServerCode/dist  # Ensure dist directory exists
 cp -r ServerCode/application/static ServerCode/dist
 
-if [ -f "ServerCode/dist/BubbleScan.exe" ]; then
+if [ -f "ServerCode/dist/BubbleScan" ]; then
     echo "BubbleScan.exe successfully created in ServerCode/dist."
 else
     echo "Error: BubbleScan.exe not found in ServerCode/dist."
