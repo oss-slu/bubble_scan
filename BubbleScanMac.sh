9a7b4f38feaea74c5ae4928c1dd5d023a37b4135
mylesc <myles.correa@slu.edu>
2024-12-02 02:03:18 -0600

    Adding all fixes to Mac sh file

--- BubbleScanMac.sh
+++ BubbleScanMac.sh
@@ -23,11 +23,12 @@ pip install --no-cache-dir -r requirements.txt
 pip install werkzeug
 pip install flask
 pip install pyinstaller
+pip install opencv-python
 
 pip uninstall -y pathlib 2>/dev/null || echo "pathlib was not reinstalled."
 
 echo "Building macOS binary..."
-pyinstaller --onefile --name BubbleScan-macOS --add-data "application/static:static" --add-data "application/logging.conf:." --hidden-import=flask --hidden-import=werkzeug --hidden-import=fitz application/AppServer.py
+pyinstaller --onefile --name BubbleScan-macOS --add-data "application/static:static" --add-data "application/logging.conf:." --add-data "BubbleScan_AI:BubbleScan_AI" --hidden-import=cv2 --hidden-import=flask --hidden-import=werkzeug --hidden-import=fitz application/AppServer.py
 
 # Deactivate the virtual environment
 deactivate
