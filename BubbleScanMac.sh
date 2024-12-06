da53f9f2d3969718f4057d8de2790c1b9bfc253c
Kate Holdener <kate.holdener@slu.edu>
2024-12-06 16:46:49 -0600

    added another signing step after building a signed application

--- BubbleScanMac.sh
+++ BubbleScanMac.sh
@@ -29,9 +29,9 @@ pip uninstall -y pathlib 2>/dev/null || echo "pathlib was not reinstalled."
 
 echo "Building macOS binary..."
 
-CODESIGN=''
+CODESIGN=""
 # Check if running in GitHub workflow
-#if [[ -n "$GITHUB_ACTIONS" ]]; then
+if [[ -n "$GITHUB_ACTIONS" ]]; then
     # Running in GitHub workflow
     security create-keychain -p actions build.keychain
     security default-keychain -s build.keychain
@@ -39,13 +39,13 @@ CODESIGN=''
     security set-keychain-settings -lut 3600 build.keychain
 
     echo "$MACOS_CERTIFICATE" | base64 --decode > certificate.p12
-        security import certificate.p12 \
+    security import certificate.p12 \
           -k build.keychain \
           -P "$MACOS_CERTIFICATE_PWD" \
           -T /usr/bin/codesign
 
-    CODESIGN=--codesign-identity "Developer ID Application: Ekaterina Holdener (V4Q7X7HV6L)"
-#fi
+    CODESIGN="--codesign-identity 'Developer ID Application: Ekaterina Holdener (V4Q7X7HV6L)'"
+fi
 
 pyinstaller --onefile $(CODESIGN) --windowed --name BubbleScan-macOS --add-data "application/static:static" --add-data "application/logging.conf:." --add-data "BubbleScan_AI:BubbleScan_AI" --hidden-import=cv2 --hidden-import=flask --hidden-import=werkzeug --hidden-import=fitz application/AppServer.py
 
