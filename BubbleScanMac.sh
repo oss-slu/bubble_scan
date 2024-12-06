b88d3508029d05d16ee119637c47bce90117d842
Kate Holdener <kate.holdener@slu.edu>
2024-12-06 10:39:46 -0600

    added --codesign-identity option to pyinstaller, only when running as part of GitHub workflow

--- BubbleScanMac.sh
+++ BubbleScanMac.sh
@@ -28,7 +28,26 @@ pip install opencv-python
 pip uninstall -y pathlib 2>/dev/null || echo "pathlib was not reinstalled."
 
 echo "Building macOS binary..."
-pyinstaller --onefile --codesign-identity "Developer ID Application: Ekaterina Holdener (V4Q7X7HV6L)" --windowed --name BubbleScan-macOS --add-data "application/static:static" --add-data "application/logging.conf:." --add-data "BubbleScan_AI:BubbleScan_AI" --hidden-import=cv2 --hidden-import=flask --hidden-import=werkzeug --hidden-import=fitz application/AppServer.py
+
+CODESIGN = ''
+# Check if running in GitHub workflow
+if [[ -n "$GITHUB_ACTIONS" ]]; then
+    # Running in GitHub workflow
+    security create-keychain -p actions build.keychain
+    security default-keychain -s build.keychain
+    security unlock-keychain -p actions build.keychain
+    security set-keychain-settings -lut 3600 build.keychain
+
+    echo "$MACOS_CERTIFICATE" | base64 --decode > certificate.p12
+        security import certificate.p12 \
+          -k build.keychain \
+          -P "$MACOS_CERTIFICATE_PWD" \
+          -T /usr/bin/codesign
+
+    CODESIGN=--codesign-identity "Developer ID Application: Ekaterina Holdener (V4Q7X7HV6L)"
+fi
+
+pyinstaller --onefile $(CODESIGN) --windowed --name BubbleScan-macOS --add-data "application/static:static" --add-data "application/logging.conf:." --add-data "BubbleScan_AI:BubbleScan_AI" --hidden-import=cv2 --hidden-import=flask --hidden-import=werkzeug --hidden-import=fitz application/AppServer.py
 
 # Deactivate the virtual environment
 deactivate
